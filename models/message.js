8564167c69a4119947acde0006fa1a1b846b1117
Baraa <baraaahmed728@gmail.com>
2023-10-08 02:16:49 +0300

    Updated message model

--- models/message.js
+++ models/message.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 1,
+    minlength: [3, "message is too short"],
+    require: [true, "You cannot send empty messages"],
   },
-  sentAt: {
-    type: Date,
+  sendTo: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "Select a user to send the message to!"],
   },
 });
 const Message = mongoose.model("Message", messageSchema);
