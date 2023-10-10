0e743f7598912bc375123d73fa8c5db5d1020994
baarayy <albaraa.ah26@icloud.com>
2023-10-10 08:09:51 +0300

    Handled errors for message routes

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,80 +1,43 @@
 const Message = require("../models/message");
 const User = require("../models/User");
-const sendMessage = async (req, res, next) => {
-  try {
-    const { id, content } = req.body;
-    const user = await User.findOne({ _id: id });
-    if (!user)
-      return res.status(400).json({
-        message: "There is no user with this id",
-      });
-    const anonyMessage = await Message.insertMany({ sendTo: id, content });
-    res.status(200).json({
-      status: "Ok",
-      msg: "Message sent successfully",
-      message: anonyMessage,
-    });
-  } catch (err) {
-    res.status(400).json({
-      msg: "Couldn't send message",
-    });
-  }
-};
-const getUserMessages = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findOne({ _id: id });
-    if (!user) {
-      return res.status(404).json({
-        Error: "There is no user with this ID",
-      });
-    }
-    console.log(user);
-    const messages = await Message.find({ sendTo: user._id });
-    res.status(200).json({
-      status: "success",
-      messages,
-    });
-  } catch (err) {
-    res.status(400).json({
-      Error: "Couldn't find any messages for this user",
-      errMsg: err,
-    });
-  }
-};
-const deleteUserMessages = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findOne({ _id: id });
-    if (!user) {
-      return res.status(404).json({
-        Error: "There is no user with this ID",
-      });
-    }
-    console.log(user);
-    await Message.deleteMany({ sendTo: user._id });
-    res.status(200).json({
-      status: "Messages deleted successfully for this user",
-    });
-  } catch (err) {
-    res.status(400).json({
-      Error: "Couldn't find any messages for this user",
-      errMsg: err,
-    });
-  }
-};
-const deleteAllMessages = async (req, res, next) => {
-  try {
-    await Message.deleteMany();
-    res.json({
-      message: "all messages have been deleted",
-    });
-  } catch (err) {
-    res.json({
-      message: "Can/'t delete messages",
-    });
-  }
-};
+const catchAsync = require("../utils/catchAsyncError");
+const sendMessage = catchAsync(async (req, res, next) => {
+  const { id, content } = req.body;
+  const user = await User.findOne({ _id: id });
+  if (!user) return next(new AppError("There is no user with this id", 404));
+  const anonyMessage = await Message.insertMany({ sendTo: id, content });
+  res.status(200).json({
+    status: "Ok",
+    msg: "Message sent successfully",
+    message: anonyMessage,
+  });
+});
+const getUserMessages = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await User.findOne({ _id: id });
+  if (!user) return next(new AppError("There is no user with this id", 404));
+  console.log(user);
+  const messages = await Message.find({ sendTo: user._id });
+  res.status(200).json({
+    status: "success",
+    messages,
+  });
+});
+const deleteUserMessages = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await User.findOne({ _id: id });
+  if (!user) return next(new AppError("There is no user with this id", 404));
+  await Message.deleteMany({ sendTo: user._id });
+  res.status(200).json({
+    status: "Messages deleted successfully for this user",
+  });
+});
+const deleteAllMessages = catchAsync(async (req, res, next) => {
+  await Message.deleteMany();
+  res.json({
+    message: "all messages have been deleted",
+  });
+});
 module.exports = {
   sendMessage,
   deleteAllMessages,
