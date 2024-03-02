a64dc6b129c9dfcf0ad68fed0f1b39768c86c242
baarayy <albaraa.ah26@icloud.com>
2024-03-02 21:42:50 +0200

    add volume

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const userRouter = require("./routes/userRoute");
 const messageRouter = require("./routes/messageRoute");
 const notFound = require("./utils/notFound");
 dotenv.config();
-const DB = process.env.DATABASE;
+const DB = process.env.MONGODB_URL;
 console.log(DB);
 mongoose
   .connect(DB)
   .then(() => console.log("DB connection is successful!"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log('error connecting to db'));
 const app = express();
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
